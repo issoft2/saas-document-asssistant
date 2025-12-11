e886da0f2329464070ea324d3410a32d51ea8c6c
isaac <isaac@heliumhealth.com>
2025-12-11 21:33:13 +0100

    moved fe execution host config to environment setting

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import router from './router'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // adjust if backend URL differs
+  baseURL: import.meta.env.VITE_API_BASE_URL, 
 })
 
 export function me() {

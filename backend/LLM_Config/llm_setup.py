eac8604eea8aa0e6ce190956b699c9de55b0295c
isaac <isaac@heliumhealth.com>
2026-01-04 23:31:08 +0100

    I changed the LLM version and the prompt

--- backend/LLM_Config/llm_setup.py
+++ backend/LLM_Config/llm_setup.py
@@ -24,17 +24,26 @@ llm_client = ChatOpenAI(
 suggestion_llm_client = ChatOpenAI(
     api_key=OPENAI_API_KEY,
     base_url=OPENAI_API_BASE,
-    model="gpt-4.1",
-    temperature=0.3,
-    max_tokens=512
+    model="gpt-4.1-nano",
+    temperature=0.7,
+    max_tokens=128
 )
 
 # Streaming LLM client
 llm_client_streaming = ChatOpenAI(
     api_key=OPENAI_API_KEY,
     base_url=OPENAI_API_BASE,
-    model="gpt-4o",
+    model="gpt-4.1",
     temperature=0.2,
     max_tokens=1536,
-    streaming=False,
+    streaming=True,
+)
+
+# Formatting 
+formatter_llm_client = ChatOpenAI(
+    api_key=OPENAI_API_KEY,
+    base_url=OPENAI_API_BASE,
+    model="gpt-4o-mini",
+    temperature=0.0,
+    max_tokens=1024
 )

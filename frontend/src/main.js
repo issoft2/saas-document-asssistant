cf6700708cc9eddbad75edacd7bd34ce3e6f07e6
isaac <isaac@heliumhealth.com>
2026-01-24 23:53:01 +0100

    refine home page, added primevue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,29 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
+import { Aura } from '@primeuix/themes'          // or Lara, Nora, etc.
+
 import ToastService from 'primevue/toastservice'
 import Toast from 'primevue/toast'
 
 import App from './App.vue'
 import router from './router'
 
-// import 'primevue/resources/themes/lara-dark-indigo/theme.css'
-// or:
-// import 'primevue/resources/themes/lara-light-indigo/theme.css'
-
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
+import 'primeicons/primeicons.css'               // icons only
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue)
-app.use(ToastService)              // 1) register the toast service
 
-app.component('Toast', Toast)      // 2) register the Toast component globally
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,                               // pick one preset
+    options: {
+      darkModeSelector: '.dark'                 // optional, if you use dark mode
+    }
+  }
+})
+
+app.use(ToastService)
+app.component('Toast', Toast)
 
 app.mount('#app')
